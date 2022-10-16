43af02a893b7bc77721eaad9a63ca31b50f8aec9
R Rui <33722833+84003@users.noreply.github.com>
2022-10-16 12:22:12 +0200

    updated

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ window.onload = function() {
   $('#contentArea').css({'opacity':0.0,'transition':'opacity 0.3s ease-in'});
 }
 
-// Scroll starts
+// As scrolling starts
 $(window).on('scroll', function(){
   console.log('Scrolling active');
   $('#contentArea').css({'opacity':1.0});
 })
 
-// Scroll ends
+// As scrolling ends
 $(window).on('scrollend', function() {
   console.log('Scrolling stopped');
   $('#contentArea').css({'opacity':0.0});
